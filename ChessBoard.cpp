d28f1c5f68c73b8dfba004a410fdee2369e951c1
2024050160478XinagZhirong <1842551454@qq.com>
2026-06-10 08:51:23 +0800

    标准开局布局

--- ChessBoard.cpp
+++ ChessBoard.cpp
@@ -1,4 +1,5 @@
 #include "ChessBoard.h"
+#include "Constants.h"
 
 ChessBoard::ChessBoard()
 {
@@ -7,6 +8,7 @@ ChessBoard::ChessBoard()
 
 void ChessBoard::initialize()
 {
+    // 清空棋盘
     for(int y = 0; y < ROWS; ++y)
     {
         for(int x = 0; x < COLS; ++x)
@@ -14,6 +16,54 @@ void ChessBoard::initialize()
             board[y][x] = Piece();
         }
     }
+
+    using namespace Xiangqi;
+
+    //----------------------------------
+    // 黑方
+    //----------------------------------
+
+    board[0][0] = Piece(SIDE_BLACK, TYPE_ROOK);
+    board[0][1] = Piece(SIDE_BLACK, TYPE_HORSE);
+    board[0][2] = Piece(SIDE_BLACK, TYPE_BISHOP);
+    board[0][3] = Piece(SIDE_BLACK, TYPE_ADVISOR);
+    board[0][4] = Piece(SIDE_BLACK, TYPE_KING);
+    board[0][5] = Piece(SIDE_BLACK, TYPE_ADVISOR);
+    board[0][6] = Piece(SIDE_BLACK, TYPE_BISHOP);
+    board[0][7] = Piece(SIDE_BLACK, TYPE_HORSE);
+    board[0][8] = Piece(SIDE_BLACK, TYPE_ROOK);
+
+    board[2][1] = Piece(SIDE_BLACK, TYPE_CANNON);
+    board[2][7] = Piece(SIDE_BLACK, TYPE_CANNON);
+
+    board[3][0] = Piece(SIDE_BLACK, TYPE_PAWN);
+    board[3][2] = Piece(SIDE_BLACK, TYPE_PAWN);
+    board[3][4] = Piece(SIDE_BLACK, TYPE_PAWN);
+    board[3][6] = Piece(SIDE_BLACK, TYPE_PAWN);
+    board[3][8] = Piece(SIDE_BLACK, TYPE_PAWN);
+
+    //----------------------------------
+    // 红方
+    //----------------------------------
+
+    board[9][0] = Piece(SIDE_RED, TYPE_ROOK);
+    board[9][1] = Piece(SIDE_RED, TYPE_HORSE);
+    board[9][2] = Piece(SIDE_RED, TYPE_BISHOP);
+    board[9][3] = Piece(SIDE_RED, TYPE_ADVISOR);
+    board[9][4] = Piece(SIDE_RED, TYPE_KING);
+    board[9][5] = Piece(SIDE_RED, TYPE_ADVISOR);
+    board[9][6] = Piece(SIDE_RED, TYPE_BISHOP);
+    board[9][7] = Piece(SIDE_RED, TYPE_HORSE);
+    board[9][8] = Piece(SIDE_RED, TYPE_ROOK);
+
+    board[7][1] = Piece(SIDE_RED, TYPE_CANNON);
+    board[7][7] = Piece(SIDE_RED, TYPE_CANNON);
+
+    board[6][0] = Piece(SIDE_RED, TYPE_PAWN);
+    board[6][2] = Piece(SIDE_RED, TYPE_PAWN);
+    board[6][4] = Piece(SIDE_RED, TYPE_PAWN);
+    board[6][6] = Piece(SIDE_RED, TYPE_PAWN);
+    board[6][8] = Piece(SIDE_RED, TYPE_PAWN);
 }
 
 bool ChessBoard::inBoard(
--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
-
 #include "RuleEngine.h"
 
 int main()
 {
     RuleEngine engine;
 
-    Piece p = engine.queryPiece(4,4);
+    Piece p1 = engine.queryPiece(0,0);
+    Piece p2 = engine.queryPiece(4,0);
+    Piece p3 = engine.queryPiece(4,9);
+    Piece p4 = engine.queryPiece(1,7);
 
     std::cout
-        << "side = "
-        << p.side
-        << " type = "
-        << p.type
-        << std::endl;
+        << p1.side << " "
+        << p1.type << std::endl;
+
+    std::cout
+        << p2.side << " "
+        << p2.type << std::endl;
+
+    std::cout
+        << p3.side << " "
+        << p3.type << std::endl;
+
+    std::cout
+        << p4.side << " "
+        << p4.type << std::endl;
 
     return 0;
 }
